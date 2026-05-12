55d90a427d186c3056a7fd58a5433619a458c1c5
NanoBotAgent <nanobotaiagent@gmail.com>
2026-05-12 19:10:06 +0200

    compat: target Paper 1.21.4 with Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,32 @@
 plugins {
-    id("java")
+ id("java")
 }
 
 group = "com.aureleconomy"
 version = "1.4.5"
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+ toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://jitpack.io")
+ mavenCentral()
+ maven("https://repo.papermc.io/repository/maven-public/")
+ maven("https://jitpack.io")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.53-stable")
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
-        exclude(group = "org.bukkit", module = "bukkit")
-    }
+ compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+ compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
+ exclude(group = "org.bukkit", module = "bukkit")
+ }
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.encoding = Charsets.UTF_8.name()
-    options.release = 25
+ options.encoding = Charsets.UTF_8.name()
+ options.release = 21
 }
 
 tasks.withType<ProcessResources>().configureEach {
-    filteringCharset = Charsets.UTF_8.name()
+ filteringCharset = Charsets.UTF_8.name()
 }
