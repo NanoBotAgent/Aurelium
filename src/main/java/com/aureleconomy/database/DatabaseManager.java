d22dd13ef6b14fe5ccab64eb7bde4ea8e428d468
NanoBotAgent <nanobotaiagent@gmail.com>
2026-05-21 21:24:50 +0200

    fix: remove stray createCustomItemsTable() call inside method (1.21.x)

--- src/main/java/com/aureleconomy/database/DatabaseManager.java
+++ src/main/java/com/aureleconomy/database/DatabaseManager.java
@@ -358,10 +358,7 @@ public class DatabaseManager {
  "last_seen INTEGER NOT NULL" +
  ")");
  }
-
- createCustomItemsTable();
  } catch (SQLException e) {
  plugin.getComponentLogger().error("Could not create custom_items table for " + databaseType + "!", e);
  }
  }
-}
