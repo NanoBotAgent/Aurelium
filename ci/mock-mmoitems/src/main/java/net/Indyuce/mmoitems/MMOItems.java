a04e26132d09de67316cb241e4cc9dacbf0707b3
NanoBotAgent <nanobotaiagent@gmail.com>
2026-06-06 22:25:27 +0200

    fix: extend JavaPlugin and add static API methods for scanner compatibility

--- ci/mock-mmoitems/src/main/java/net/Indyuce/mmoitems/MMOItems.java
+++ ci/mock-mmoitems/src/main/java/net/Indyuce/mmoitems/MMOItems.java
@@ -2,20 +2,22 @@ package net.indyuce.mmoitems;
 
 import net.indyuce.mmoitems.api.Type;
 import net.indyuce.mmoitems.manager.ItemManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class MMOItems {
+public class MMOItems extends JavaPlugin {
     private static MMOItems instance;
     private final ItemManager itemManager;
     private final Map<String, Object> items = new HashMap<>();
 
-    public MMOItems(ItemManager itemManager) {
-        this.itemManager = itemManager;
+    public MMOItems() {
+        this.itemManager = new ItemManager();
         instance = this;
     }
 
-    public static MMOItems getInstance() {
+    public static MMOItems getPlugin() {
         return instance;
     }
 
@@ -29,5 +31,32 @@ public class MMOItems {
 
     public boolean hasItem(String key) {
         return items.containsKey(key.toLowerCase());
+    }
+
+    // Bootstrap method to register default items
+    public static void registerDefaults() {
+        if (instance == null) new MMOItems();
+        ItemManager itemManager = instance.getItems();
+        
+        // Register types
+        Type swordType = new Type("SWORD", "weapon");
+        Type pickaxeType = new Type("PICKAXE", "tool");
+        Type axeType = new Type("AXE", "tool");
+        
+        // Register items
+        itemManager.register(new MMOItem("RUBY_BLADE", swordType));
+        itemManager.register(new MMOItem("SAPPHIRE_PICKAXE", pickaxeType));
+        itemManager.register(new MMOItem("EMERALD_AXE", axeType));
+    }
+
+    @Override
+    public void onEnable() {
+        registerDefaults();
+        getLogger().info("MockMMOItems enabled with " + itemManager.getAllTypes().size() + " types!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("MockMMOItems disabled!");
     }
 }
